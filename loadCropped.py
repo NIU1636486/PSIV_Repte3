60d557dff743978419d380109d55f9549b779e55
NIU1636671 <91468386+NIU1636671@users.noreply.github.com>
2024-11-11 20:13:41 +0100

    Correcció d'errors

--- loadCropped.py
+++ loadCropped.py
@@ -12,7 +12,7 @@ import cv2
 import pandas as pd
 def loadCropped(ListFolders, nImages):
     pathExcel = "PatientDiagnosis.csv"
-    patientsImgs = np.array(0)
+    patientsImgs = list()
     excel = pd.read_csv(pathExcel)
     for pathDir in ListFolders:
         imgs = glob.glob(os.path.join(pathDir,'*.png'))
@@ -20,17 +20,25 @@ def loadCropped(ListFolders, nImages):
         for _ in range(nImages):
             index = random.randint(0,n)
             img = cv2.imread(imgs[index])
-            diagnosis = excel.loc[excel['CODI'] == pathDir, 'DENSITAT'].values[0]
+            diagnosis = excel.loc[excel['CODI'] == pathDir[:-2], 'DENSITAT'].values[0]
 
             if diagnosis == "NEGATIVA":
                 diagnosis = 0
             elif diagnosis == "BAIXA" or diagnosis == "ALTA":
                 diagnosis = 1
-            metadates = [pathDir,imgs[index],diagnosis]
-            patientsImgs.append([img,metadates])
+            metadades = [pathDir[:-2],imgs[index],diagnosis]
+            patientsImgs.append([img,metadades])
     
     return patientsImgs
 
-            
+
+# Directori Actual
+directorio_actual = os.getcwd()
+
+# Llistar les carpetes del directori actual
+carpetas = [nombre for nombre in os.listdir(directorio_actual) if os.path.isdir(os.path.join(directorio_actual, nombre))]
+
+patientImgs = loadCropped(carpetas,20)
+
     
     

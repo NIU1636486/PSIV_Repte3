37a12112f114781e026a7bd6c77668cb8e297c1d
NIU1636671 <91468386+NIU1636671@users.noreply.github.com>
2024-11-21 23:56:40 +0100

    Patient Diagnosis + Validació

--- Validation_KFolds_PatchClassification.py
+++ Validation_KFolds_PatchClassification.py
@@ -1,4 +1,5 @@
 # -*- coding: utf-8 -*-
+
 """
 Created on Tue Nov 19 12:59:58 2024
 
@@ -47,7 +48,7 @@ import torch.optim as optim
 ## Own Functions
 from Models.AEmodels import AutoEncoderCNN
 from Models.datasets import Standard_Dataset
-#from loadCropped import loadCropped
+from loadCropped import loadCropped
 from loadannotated import loadAnnotated
 from reconstructionerror import *
 
@@ -155,6 +156,8 @@ model.load_state_dict(torch.load('model_rapid_nou.pth'))
 model.eval()  # Set the model to evaluation mode
 
 ################ CARREGUEM LES DADES ###################
+ListFolders = glob.glob("Cropped/*")
+patientsImgsCropped, patientsMetaCropped = loadCropped(ListFolders,10)
 
 ListFolders = glob.glob("Annotated/*")
 patientsImgs, patientsMeta = loadAnnotated(ListFolders, 10)
@@ -180,12 +183,12 @@ model.load_state_dict(torch.load('model_rapid_nou.pth'))
 model.eval()  # Set the model to evaluation mode
 
 
-
+print("\nStarting with Patch classification...")
 folds_optimals_tresholds = list()
 patientsID = [x[0] for x in patientsMeta]
 y = [x[2] for x in patientsMeta]
 X = range(len(patientsImgs))
-kf = GroupKFold(n_splits=10)
+kf = GroupKFold(n_splits=2)
 folds = list()
 
 for fold, (train_idx, val_idx) in enumerate(kf.split(X, y, patientsID)):
@@ -196,16 +199,24 @@ for fold, (train_idx, val_idx) in enumerate(kf.split(X, y, patientsID)):
     errors = []  # List to store reconstruction errors
     labels = []  # List to store true labels
     
+    # Recuperem les imatges train
     train_imgs = [patientsImgs[i] for i in train_idx]
+    # Recuperem les imatges test
     test_imgs = [patientsImgs[i] for i in val_idx]
-    
-    train_meta = [y[i] for i in train_idx]
+    # Recuperem la label de les imatges train
+    train_label = [y[i] for i in train_idx]
+    # Recuperem els patients utilitzats pel train del fold
+    train_patients = list(set([patientsMeta[i][0] for i in train_idx]))
+    # Recuperem els patients utilitzats pel test del fold
+    test_patients = list(set([patientsMeta[i][0] for i in val_idx]))
+    # Recuperem les metadades del conjunt de validació
     test_meta = [patientsMeta[i] for i in val_idx]
-    folds.append([train_imgs,test_imgs,train_meta,test_meta])
+    # Afegim a la llista de folds els pacients utilitzats
+    folds.append([train_patients,test_patients])
     
     # Train reconstruction error
     print(f'\nTraining Fold {fold}....')
-    for input_image, label in zip(train_imgs,train_meta):
+    for input_image, label in zip(train_imgs,train_label):
         
         input_image = cv2.cvtColor(input_image, cv2.COLOR_BGR2RGB)
         input_image = input_image.astype(np.float32) / 255.0
@@ -247,11 +258,11 @@ for fold, (train_idx, val_idx) in enumerate(kf.split(X, y, patientsID)):
     plt.show()
     folds_optimals_tresholds.append([optimal_threshold, fpr, tpr])
     
-    positive = {}
-    total = 0
+    
+
     print(f'\nValidation Fold {fold}....')
+    patch_classification = []
     for input_image, meta in zip(test_imgs,test_meta):
-        
         patID, nameImg, label = meta
         
         input_image = cv2.cvtColor(input_image, cv2.COLOR_BGR2RGB)
@@ -271,17 +282,123 @@ for fold, (train_idx, val_idx) in enumerate(kf.split(X, y, patientsID)):
         reconstruction_error = compute_reconstruction_error(input_image, output_image)
         f_red = calculate_f_red(input_image, output_image)
         
+        if f_red >= optimal_threshold:
+            patch_classification.append([1,label])
+        
+        else:
+            patch_classification.append([-1,label])
+
+print("\nStarting with Patient Diagnosis....")
+
+folds_optimals_diagnosis = list()
+diagnosis = {}
+for fold, (train_pat, val_pat) in enumerate(folds):
+    patientsDiagnosisImgs_train = list()
+    patientsDiagnosisMeta_train = list()
+    patientsDiagnosisImgs_test = list()
+    patientsDiagnosisMeta_test = list()
+
+    
+    for x,y in zip(patientsImgsCropped, patientsMetaCropped):
+        
+        if y[0] in train_pat:
+            patientsDiagnosisImgs_train.append(x)
+            patientsDiagnosisMeta_train.append(y)
+            if y[0] not in diagnosis:
+                diagnosis[y[0]] = y[2]
+        
+        elif y[0] in val_pat:
+            patientsDiagnosisImgs_test.append(x)
+            patientsDiagnosisMeta_test.append(y)
+    errors = list()
+    labels = list()
+    positive = {}
+    print(f"\nTraining fold {fold}...")
+    optimalTreshold = folds_optimals_tresholds[fold]
+    for input_image, meta in zip(patientsDiagnosisImgs_train,patientsDiagnosisMeta_train):      
+         
+        patID, nameImg, label = meta
+       
+        input_tensor = transform(input_image).unsqueeze(0)
+        
+        # Pass the image through the model
+        with torch.no_grad():
+            output_tensor = model(input_tensor)
+        
+        # Postprocess the output image
+        output_image = output_tensor.squeeze(0).detach().cpu().numpy()  # Remove batch dimension
+        output_image = np.transpose(output_image, (1, 2, 0))  # Convert to HWC format
+        
+        # Compute reconstruction error and F_red
+        reconstruction_error = compute_reconstruction_error(input_image, output_image)
+        f_red = calculate_f_red(input_image, output_image)
+        
+
         if patID not in positive:
             positive[patID] = 0
             
         if f_red > optimal_threshold:
             positive[patID] += 1
         
-      
-    
+
     for key,value in positive.items():
-        print(f"Percentatge de finestres positives pel pacient {key} =",(value/10)*100,"%")
- 
-
+        diag = (value/10)
+        errors.append(diag)
+        labels.append(diagnosis[key])
+        print(f"Percentatge de finestres positives pel pacient {key} =",diag*100,"%")
     
+    
+    # Perform ROC analysis
+    optimal_diagnosis, fpr, tpr = roc_threshold_analysis(errors, labels)
+    print(f"Optimal Threshold: {optimal_threshold}")
+    
+    # Plot the ROC curve
+    plt.figure()
+    plt.plot(fpr, tpr, label="ROC Curve")
+    plt.scatter([0], [1], color="red", label="Ideal Point")
+    plt.xlabel("False Positive Rate")
+    plt.ylabel("True Positive Rate")
+    plt.title("ROC Curve")
+    plt.legend()
+    plt.show()
+    folds_optimals_diagnosis.append([optimal_diagnosis, fpr, tpr])
+    
+    print(f'\nValidation Fold {fold}....')
+    patient_diagnosis = []
+    positive = {}
+    for input_image, meta in zip(patientsDiagnosisImgs_test, patientsDiagnosisMeta_test):
+        patID, nameImg, label = meta
+        
+            
+        input_tensor = transform(input_image).unsqueeze(0)
+        
+        # Pass the image through the model
+        with torch.no_grad():
+            output_tensor = model(input_tensor)
+        
+        # Postprocess the output image
+        output_image = output_tensor.squeeze(0).detach().cpu().numpy()  # Remove batch dimension
+        output_image = np.transpose(output_image, (1, 2, 0))  # Convert to HWC format
+        
+        # Compute reconstruction error and F_red
+        reconstruction_error = compute_reconstruction_error(input_image, output_image)
+        f_red = calculate_f_red(input_image, output_image)
+        
 
+        if patID not in positive:
+            positive[patID] = 0
+            
+        if f_red > optimal_threshold:
+            positive[patID] += 1
+        
+    for key,value in positive.items():
+        diag = (value/10)
+        print(f"Percentatge de finestres positives pel pacient {key} =",diag*100,"%")
+        if diag >= optimal_diagnosis:
+            patient_diagnosis.append([1,diagnosis[key]])
+            print(f"El pacient {key} té la presencia de l'helicobacter")
+        
+        else:
+            patient_diagnosis.append([-1,diagnosis[key]])
+            print(f"El pacient {key} no té la presencia de l'helicobacter")
+            

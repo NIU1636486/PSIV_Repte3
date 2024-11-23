d9492e0d00fcb745ebdd37a2db784ac6d0d28f57
NIU1636671 <91468386+NIU1636671@users.noreply.github.com>
2024-11-23 19:17:13 +0100

    Funcions per executar la validació al servidor

---
+++ loadAnnotatedServidor.py
@@ -0,0 +1,95 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Nov 23 19:06:07 2024
+
+@author: marcp
+"""
+
+import glob
+import os
+import random
+import cv2
+import pandas as pd
+import numpy as np
+import re
+
+#tenim un excel HP_WSI-CoordAllAnnotatedPatches.xlsx" amb:
+#Pat_ID: B22-129
+#Section_ID: 0
+#ON PAT_ID_SECTION_ID ÉS EL NOM DE LA CARPETA AMB LA IMATGE AMB .PNG
+#ON EL FILENAME ËS WINDOW_ID. 
+
+def loadAnnotated(ListFolders, nImg):
+    
+    pathExcel = "/fhome/maed/HelicoDataSet/HP_WSI-CoordAllAnnotatedPatches.xlsx"
+    excel = pd.read_excel(pathExcel)
+    # ELIMINAR SI WINDOW ID CONTE number_Aug???
+    excel = excel[~excel['Window_ID'].astype(str).str.contains('_Aug', regex=False)]
+    # nomes numerics i passar-los a integer per no llegir-los com a float
+    excel = excel[excel['Window_ID'].apply(lambda x: str(x).isdigit())]
+    excel['Window_ID'] = excel['Window_ID'].astype(int)
+    
+    annotatedImgs = []
+    annotatedtMeta= []
+    nImages = nImg
+    
+    for pathDir in ListFolders:
+        # extreure Pat_ID i Section_ID del nom de la carpeta
+        folder_name = os.path.basename(pathDir)
+        pat_id, section_id = folder_name.split('_')
+        
+        # filtrar del excel els que coincideixen amb pat id i section id carpeta
+        folder_data = excel[(excel['Pat_ID'] == pat_id) & (excel['Section_ID'] == int(section_id))]
+        
+        pathdir_llarg = os.path.join("/fhome/maed/HelicoDataSet/CrossValidation/Annotated/", pathDir)
+        
+        pathimgs = os.path.join(pathdir_llarg,'*.png')
+        imgs = glob.glob(pathimgs)
+        n = len(imgs)
+        
+        if nImg is None:
+            nImages = n
+
+        for i in range(min(nImages, n)):
+            # seleccionar una imatge de manera random
+        
+            img_path = imgs[i]
+            img = cv2.imread(img_path)
+
+            # extreure filename sense el .png per fer match amb Window_ID del excel
+            img_filename = os.path.basename(img_path).replace('.png', '')
+            
+            # Use regex to extract only the numeric part of the filename (Window_ID)
+            match = re.match(r"(\d+)", img_filename)
+
+            if not match:
+                print(f"Filename '{img_filename}.png' does not match expected pattern. Skipping.")
+                continue
+            
+            # Convert the numeric part to an integer
+            img_window_id = int(match.group(1))
+            
+            
+            # fer match Window_ID amb folder data
+            presence_row = folder_data[folder_data['Window_ID'] == img_window_id]
+            if presence_row.empty:
+                print(f"No matching Window_ID for image '{img_filename}.png' in Excel data. Skipping.")
+                continue
+            
+            #extreure valor presence
+            presence = presence_row['Presence'].values[0]
+            if presence != 0:
+                #afegir a les dues llistes
+                annotatedImgs.append(img)
+                annotatedtMeta.append([pat_id, img_window_id, presence])
+
+    return annotatedImgs, annotatedtMeta
+
+#ListFolders = glob.glob("/Users/carlotacortes/Desktop/Annotated/*")
+
+#patientsImgs, patientsMeta  = loadAnnotated(ListFolders, nImages)
+
+# Print the results for verification
+#for img, metadata in zip(patientsImgs, patientsMeta):
+    #print(img)
+    #print("Metadata:", metadata)
---
+++ loadCroppedServidor.py
@@ -0,0 +1,58 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Nov 23 18:58:27 2024
+
+@author: marcp
+"""
+import glob
+import os
+import numpy as np
+import random
+import cv2
+import pandas as pd
+import skimage.io as io
+
+def loadCropped(ListFolders, nImgs):
+    pathExcel = "/fhome/maed/HelicoDataSet/PatientDiagnosis.csv"
+    patientsImgs = list()
+    patientsMeta= []
+    excel = pd.read_csv(pathExcel)
+    nImages = nImgs
+
+    for pathDir in ListFolders:
+        print("Loading images from", pathDir)
+        pathdir_llarg = os.path.join("/fhome/maed/HelicoDataSet/CrossValidation/Cropped/", pathDir)
+        imgs = os.path.join(pathdir_llarg,'*.png')
+        imgs = glob.glob(imgs)
+        n = len(imgs)
+        
+        if nImgs == None:
+            nImages = n
+            
+        this_patient = 0	
+        while this_patient < min(nImages,n):
+            index = random.randint(0,len(imgs)-1)
+            img = cv2.imread(imgs[index])
+            if img.shape != (256, 256, 3):
+                imgs.pop(index)
+                if len(imgs) == 0:
+                    break
+                print("Image with wrong size found", img.size)
+                continue
+            img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB) if img.shape[-1] >= 3 else img[:, :, :3]
+            diagnosis = excel.loc[excel['CODI'] == pathDir[:-2], 'DENSITAT'].values[0]
+
+            if diagnosis == "NEGATIVA":
+                diagnosis = -1
+            elif diagnosis == "BAIXA" or diagnosis == "ALTA":
+                diagnosis = 1
+            patientsMeta.append([pathDir[:-2],imgs[index],diagnosis])
+	 		# transpose img
+            img = np.transpose(img, (2, 0, 1))
+			#img = img.astype(np.float32)
+            img = img.astype(np.float32)
+            img = img/255.0
+            patientsImgs.append(img)
+            this_patient += 1
+
+    return patientsImgs, patientsMeta

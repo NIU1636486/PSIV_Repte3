d308bc30d5116038a944f780edf60306baf6cd17
NIU1636671 <91468386+NIU1636671@users.noreply.github.com>
2024-11-27 17:22:47 +0100

    Add files via upload

--- loadCroppedServidor.py
+++ loadCroppedServidor.py
@@ -22,17 +22,28 @@ def loadCropped(ListFolders, nImgs):
     for pathDir in ListFolders:
         print("Loading images from", pathDir)
         pathdir_llarg = os.path.join("/fhome/maed/HelicoDataSet/CrossValidation/Cropped/", pathDir)
-        imgs = os.path.join(pathdir_llarg,'*.png')
-        imgs = glob.glob(imgs)
+        imgs = os.listdir(pathdir_llarg)
         n = len(imgs)
-        
+        print(n)
         if nImgs == None:
             nImages = n
             
         this_patient = 0	
         while this_patient < min(nImages,n):
+            print(len(imgs))
+            if len(imgs) == 0:
+                break
             index = random.randint(0,len(imgs)-1)
-            img = cv2.imread(imgs[index])
+            imgname = imgs.pop(index)
+            if imgname[-3:] != "png":
+                continue
+            
+            imgpath = os.path.join(pathdir_llarg,imgname)
+            
+           
+            
+            img = cv2.imread(imgpath)
+            print("imatge carregada")
             if img.shape != (256, 256, 3):
                 imgs.pop(index)
                 if len(imgs) == 0:
@@ -46,7 +57,8 @@ def loadCropped(ListFolders, nImgs):
                 diagnosis = -1
             elif diagnosis == "BAIXA" or diagnosis == "ALTA":
                 diagnosis = 1
-            patientsMeta.append([pathDir[:-2],imgs[index],diagnosis])
+            
+            patientsMeta.append([pathDir[:-2],imgname,diagnosis])
 	 		# transpose img
             img = np.transpose(img, (2, 0, 1))
 			#img = img.astype(np.float32)
